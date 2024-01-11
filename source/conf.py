df9fccff7a54a738405a7ab6b28f906b30f958d7
YuShiba <shiyushibaya@amail.com>
2024-01-11 10:40:01 +0800

    daily commit

---
+++ source/conf.py
@@ -0,0 +1,60 @@
+# -*- encoding: utf-8 -*-
+
+"""
+
+@File    :   conf.py
+@Author  :   Yushiba972
+@description  :   source url - https://www.sphinx-doc.org/en/master/usage/configuration.html 
+
+"""  
+
+from msvcrt import kbhit
+from recommonmark.parser import CommonMarkParser
+
+
+# -- 1. 项目信息 -----------------------------------------------------
+
+project = 'Notes'
+copyright = '2024, Arthur'
+author = 'Arthur'
+release = 'dev'
+
+
+
+# -- 2. 拓展的python module模块 -----------------------------------------------------
+
+extensions = [    
+     'recommonmark',  # markdown语法支持插件配合sphinx_markdown_tables使用
+    #  'sphinx_markdown_tables' # markdown语法支持插件
+ ]
+
+
+
+# -- 3. 模板路径，相对conf.py的位置 -----------------------------------------------------
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- 4. HTML输出渲染 -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'     # 主题
+html_static_path = ['_static']  # 静态资源相对位置
+
+
+
+# -- 5. 其他配置 -------------------------------------------------
+# 指定后缀文件使用的解析器
+source_parsers = {
+    '.md': CommonMarkParser,
+    '.MD': CommonMarkParser,
+}
+
+# 指定后缀文件识别格式
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+# # sphinx识别的源文件后缀，此处表示识别.rst和.md后缀的文件为项目源文件
+# source_suffix = ['.rst', '.md', '.MD']
---
+++ source/index.rst
@@ -0,0 +1,23 @@
+.. note documentation master file, created by
+   sphinx-quickstart on Wed Jan 10 17:57:42 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+欢迎来到Yushiba972的笔记仓库
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 目录
+
+   conda/index.rst
+
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
